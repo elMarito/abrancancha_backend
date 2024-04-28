a185fc4906ad9a928c6486c3041b59070435ab40
JuanTomasL <juantomaslacave@gmail.com>
2024-04-28 00:45:09 -0300

    Tariff sin probar

--- src/tariff/tariff.service.ts
+++ src/tariff/tariff.service.ts
@@ -1,23 +1,120 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateTariffDto } from './dto/create-tariff.dto';
 import { UpdateTariffDto } from './dto/update-tariff.dto';
+import { Tariff } from './entities/tariff.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { FindOneOptions, Repository } from 'typeorm';
+
 
 @Injectable()
 export class TariffService {
-  create(createTariffDto: CreateTariffDto) {
-    return 'This action adds a new tariff';
+  private tariffs: Tariff[] = [];
+  
+  constructor(
+    @InjectRepository(Tariff)
+    private readonly tariffRepository:Repository<Tariff>){}
+
+    
+    //---------------------------------------------------------------------------------------
+    public async getAll(): Promise<Tariff[]> {
+      try {
+        this.tariffs = await this.tariffRepository.find();
+        if (this.tariffs)
+          return this.tariffs
+        else throw new Error('Error in the query Tariff is not found');
+      } catch (error) {
+        throw new HttpException({
+          status: HttpStatus.NOT_FOUND, error: "Search error" + error
+        }, HttpStatus.NOT_FOUND)
+      }
+    }
+    public async findOne(id: number): Promise<Tariff[]> {
+        try {
+          const criterio: FindOneOptions = { where:{ id: id }};
+          let tariff: Tariff = await this.tariffRepository.findOne(criterio);
+          if (tariff){
+            this.tariffs = [];
+            this.tariffs.push(tariff)
+          }
+          else throw new Error('No tariff found');
+          return this.tariffs;
+        } catch (error) {
+          throw new HttpException({
+            status: HttpStatus.NOT_FOUND, error: "Fare search error :" + error
+          }, HttpStatus.NOT_FOUND)
+    
+        }
+      }  
+  
+    //------------------------------------------------------------------------
+    public async getById(id : number) : Promise<Tariff[]> {
+      try {
+          const criterio : FindOneOptions = { relations: [ 'courts' ], where: { id: id } }
+          let tariff : Tariff = await this.tariffRepository.findOne( criterio );
+          this.tariffs = [];
+          if (tariff) 
+              this.tariffs.push(tariff);
+          else
+              throw new Error('No tariff found')
+          return this.tariffs;
+      } catch (error) {
+          throw new HttpException( {
+              status : HttpStatus.NOT_FOUND, error : 'Fare search error ' + id + ' : ' + error 
+          }, HttpStatus.NOT_FOUND);
+      }
   }
+
+  public async create(createTariffDto: CreateTariffDto): Promise<Tariff> {
+    try {
+      let tariff: Tariff = await this.tariffRepository.save(new Tariff  (
+        createTariffDto.name, createTariffDto.price));
+      if (tariff)
+        return tariff;
+      else
+        throw new DOMException('Error, the tariff could not be created ');
+
+    } catch (error) {
+      throw new HttpException({
+        status: HttpStatus.NOT_FOUND,
+        error: 'Error, the tariff could not be created '+ error
+      }, HttpStatus.NOT_FOUND);
+    }
+
+  }
+
+  public async updateTariff(TariffDTO : UpdateTariffDto) : Promise<Tariff> {
+    try {
+        let criterio : FindOneOptions = { where:{id:TariffDTO.id}};
+        let tariff : Tariff = await this.tariffRepository.findOne(criterio);
+        if (!tariff)
+           throw new DOMException('The tariff was not found');
+        else
+           tariff.setName(TariffDTO.name);
+        tariff = await this.tariffRepository.save(tariff);
+        return tariff;
+     } catch (error) {
+           throw new HttpException( { status : HttpStatus.NOT_FOUND, 
+                 error : 'Tariff update error '+error}, HttpStatus.NOT_FOUND);
+     }
+     }
+
+     public async deleteTariff(id:number) : Promise<boolean> {
+      try {
+         let criterio : FindOneOptions = {where:{id:id}};
+         let tariff : Tariff = await this.tariffRepository.findOne(criterio);
+         if (!tariff)
+            throw new DOMException('The tariff was not found');
+         else
+            await this.tariffRepository.delete(tariff.getId());
+         return (true)
+      } catch (error) {
+            throw new HttpException( { status : HttpStatus.NOT_FOUND, 
+                  error : 'Tariff elimination error '+error}, HttpStatus.NOT_FOUND);
+      }
+    }   
 
   findAll() {
     return `This action returns all tariff`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} tariff`;
-  }
-
-  update(id: number, updateTariffDto: UpdateTariffDto) {
-    return `This action updates a #${id} tariff`;
   }
 
   remove(id: number) {
