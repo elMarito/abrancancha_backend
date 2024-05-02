5221c78b86494a8e54df9aaa258a73b6a8415d00
Mario Patronelli <mar.ioepatronelli@gmail.com>
2024-05-02 20:56:22 -0300

    Actualizacion de la entidad user

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,7 +1,13 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { Column } from 'typeorm';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
-    // readonly idCiudad : number;
-    readonly fullname : string;
+  // readonly
+
+  @Column()
+  readonly phone?: string;
+
+  @Column()
+  readonly avatar?: string;
 }
