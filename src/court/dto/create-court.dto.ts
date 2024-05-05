fd3698696fb051e12196956b749ad451bf95886c
Nicolas-Mansilla <nicolasmasnsilla6284@gmail.com>
2024-05-05 11:39:53 -0300

    intente crear los servicios del court, no logre el update ni el delete. Falta terminar

--- src/court/dto/create-court.dto.ts
+++ src/court/dto/create-court.dto.ts
@@ -1,1 +1,15 @@
-export class CreateCourtDto {}
+import { Type } from "@nestjs/common";
+import { Tariff } from "src/tariff/entities/tariff.entity";
+import { TypeOfCourt } from "src/type-of-court/entities/type-of-court.entity";
+
+export class CreateCourtDto {
+    readonly numb: string;
+    readonly name:string;
+    readonly idTariff:number;
+    readonly idType:number;
+    readonly idTimetable:number;
+    readonly rating:string;
+    readonly observations:string;
+    readonly idStatus:number;
+    
+}
