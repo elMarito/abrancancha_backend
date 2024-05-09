bf36eba5d5d8c2b9dc3ef83ca8d98568c7f1046b
Mario Patronelli <mar.ioepatronelli@gmail.com>
2024-05-08 22:24:28 -0300

    correciones de imports y reformateo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+//---------entidades ▼
 import { UserModule } from './user/user.module';
 import { AdministratorModule } from './administrator/administrator.module';
 import { ClubModule } from './club/club.module';
 import { ReservationModule } from './reservation/reservation.module';
 import { TypeOfCourtModule } from './type-of-court/type-of-court.module';
 import { CourtModule } from './court/court.module';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { join } from 'path';
 import { StatusOfUserModule } from './status-of-user/status-of-user.module';
 import { TariffModule } from './tariff/tariff.module';
 import { TimetableModule } from './timetable/timetable.module';
@@ -18,8 +19,11 @@ import { StatusOfCourtModule } from './status-of-court/status-of-court.module';
 import { ExceptionTimedateModule } from './exception-timedate/exception-timedate.module';
 import { ScheduleModule } from './schedule/schedule.module';
 import { AuthModule } from './auth/auth.module';
-import { StatusOfUser } from './status-of-user/entities/status-of-user.entity';
-import { User } from './user/entities/user.entity';
+// import { StatusOfUser } from './status-of-user/entities/status-of-user.entity';
+// import { User } from './user/entities/user.entity';
+import { APP_GUARD } from '@nestjs/core';
+import { RolesGuard } from './auth/roles.guard';
+//https://docs.nestjs.com/guards
 
 @Module({
   imports: [
@@ -30,12 +34,11 @@ import { User } from './user/entities/user.entity';
       port: 3306,
       username: 'root',
       password: 'root',
-      database: 'abrancancha',
-      entities: [        
-        "dist/**/**.entity{.ts,.js}"
-      ],
+      database: 'abranCancha',
+      entities: ['dist/**/**.entity{.ts,.js}'],
       synchronize: false,
     }),
+    AuthModule,
     UserModule,
     AdministratorModule,
     ClubModule,
@@ -49,9 +52,13 @@ import { User } from './user/entities/user.entity';
     StatusOfCourtModule,
     ExceptionTimedateModule,
     ScheduleModule,
-    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
+  // providers: [ AppService, AuthGuard],
+  // providers: [{
+  //   provide: APP_GUARD,
+  //   useClass: RolesGuard,
+  // }, AppService],
 })
 export class AppModule {}
