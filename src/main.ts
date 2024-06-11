b4d8b7d10985ac62f5f6944087dc8eff668e3f8c
Mario Patronelli <64616591+elMarito@users.noreply.github.com>
2024-06-11 00:01:14 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './http-exception.filter';
- import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 // import { RolesGuard } from './auth/roles.guard';
 // import { AuthGuard } from './auth/auth.guard';
 
@@ -15,14 +15,14 @@ async function bootstrap() {
   app.useGlobalFilters(new HttpExceptionFilter());
   //  app.useGlobalGuards(new AuthGuard());
   // app.useGlobalGuards(new RolesGuard())
-//   const options = new DocumentBuilder()
-//   .setTitle('Abran Cancha')
-//   .setDescription('Api para la conexion a la DB de Abran Cancha')
-//   .setVersion('1.0')
-//   .addTag('abrancancha')
-//   .build();
-// const document = SwaggerModule.createDocument(app, options);
-// SwaggerModule.setup('api', app, document);
+   const options = new DocumentBuilder()
+   .setTitle('Abran Cancha')
+   .setDescription('Api para la conexion a la DB de Abran Cancha')
+   .setVersion('1.0')
+   .addTag('abrancancha')
+   .build();
+ const document = SwaggerModule.createDocument(app, options);
+ SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
